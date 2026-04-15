7daefcd417ca88c5eef321298ce7a5796592d32e
monaallam62 <monaallam5102@gmail.com>
2026-04-15 23:15:16 +0200

    Question  2

--- Assign_LINQ_2/Program.cs
+++ Assign_LINQ_2/Program.cs
@@ -1,5 +1,6 @@
-﻿using static Assign_LINQ_2.DataSource.Source;
+﻿using Assign_LINQ_2.DataSource;
 using Assign_LINQ_2.Models;
+using static Assign_LINQ_2.DataSource.Source;
 namespace Assign_LINQ_2
 {
     internal class Program
@@ -13,6 +14,16 @@ namespace Assign_LINQ_2
             //foreach (var product in top3Expensive)
             //    Console.WriteLine($"{product.ProductName}: ${product.UnitPrice}");
             #endregion
+            #region Question 2
+            //int pageSize = 5;
+            //int pageNumber = 2;
+            //var pagedProducts =ProductList
+            //    .Skip((pageNumber - 1) * pageSize)
+            //    .Take(pageSize);
+            //foreach (var product in pagedProducts)
+            //    Console.WriteLine($"{product.ProductID}: {product.ProductName} - ${product.UnitPrice}");
+            #endregion
+
         }
     }
 }
