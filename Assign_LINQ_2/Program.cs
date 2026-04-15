191661c996c18d4b284df258c7adfd4635fa8f8d
monaallam62 <monaallam5102@gmail.com>
2026-04-15 23:14:23 +0200

    Question 1

--- Assign_LINQ_2/Program.cs
+++ Assign_LINQ_2/Program.cs
@@ -7,11 +7,11 @@ namespace Assign_LINQ_2
         static void Main(string[] args)
         {
             #region Qustion 1
-            var top3Expensive = ProductList
-                .OrderByDescending(p => p.UnitPrice)
-                .Take(3);
-            foreach (var product in top3Expensive)
-                Console.WriteLine($"{product.ProductName}: ${product.UnitPrice}");
+            //var top3Expensive = ProductList
+            //    .OrderByDescending(p => p.UnitPrice)
+            //    .Take(3);
+            //foreach (var product in top3Expensive)
+            //    Console.WriteLine($"{product.ProductName}: ${product.UnitPrice}");
             #endregion
         }
     }
