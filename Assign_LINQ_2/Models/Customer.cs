4fdb456b25f177913600448aa8604de8b46a34e2
monaallam62 <monaallam5102@gmail.com>
2026-04-15 23:13:01 +0200

    Source

---
+++ Assign_LINQ_2/Models/Customer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Assign_LINQ_2.DataSource;
+
+namespace Assign_LINQ_2.Models
+{
+    internal class Customer
+    {
+        public string CustomerID { get; set; }
+        public string CompanyName { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string Region { get; set; }
+        public string PostalCode { get; set; }
+        public string Country { get; set; }
+        public string Phone { get; set; }
+        public Order[] Orders { get; set; }
+        public override string ToString() => $"{CustomerID} {CompanyName}\n{Address}\n{City}, {Region} {PostalCode} {Country}\n{Phone}";
+
+    }
+}
--- Assign_LINQ_2/Program.cs
+++ Assign_LINQ_2/Program.cs
@@ -1,10 +1,18 @@
-﻿namespace Assign_LINQ_2
+﻿using static Assign_LINQ_2.DataSource.Source;
+using Assign_LINQ_2.Models;
+namespace Assign_LINQ_2
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Qustion 1
+            var top3Expensive = ProductList
+                .OrderByDescending(p => p.UnitPrice)
+                .Take(3);
+            foreach (var product in top3Expensive)
+                Console.WriteLine($"{product.ProductName}: ${product.UnitPrice}");
+            #endregion
         }
     }
 }
